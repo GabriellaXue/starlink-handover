637c0fe9afb1541c180671b544c2aa1ee583f9f6
Gabriella Xue <fanxue2@illinois.edu>
2024-06-18 22:19:43 -0500

    fix mahimahi trace generation script

--- real_exp/generate_trace.py
+++ real_exp/generate_trace.py
@@ -7,19 +7,20 @@ def parse_log(file_name):
 def write_trace(file_name, data):
     duration_ms = 1000
     timestamp = 1
+    skip_flag = False
 
     with open(file_name, 'w') as file:
         for capacity in data:
-            packet_number_per_ms = capacity * 10 ** 6 / 1000 / 1504 / 7
+            packet_number_per_ms = (capacity * 10 ** 6) / 1000 / 1504
             remainder = 0
-            if packet_number_per_ms > 1 or packet_number_per_ms == 1:
+            if packet_number_per_ms >= 1:
                 for _ in range(duration_ms):
                     repeated_times = round(packet_number_per_ms)
                     remainder += packet_number_per_ms - repeated_times
-                    if remainder > 1 or remainder == 1:
+                    if remainder >= 1:
                         repeated_times += 1
                         remainder -= 1
-                    elif remainder < -1 or remainder == -1:
+                    elif remainder <= -1:
                         repeated_times -= 1
                         remainder += 1
                     for _ in range(repeated_times):
@@ -27,23 +28,22 @@ def write_trace(file_name, data):
                     timestamp += 1
             else:
                 for _ in range(duration_ms):
-                    repeated_times = 1
-                    timestamp += round(packet_number_per_ms)
-                    remainder += packet_number_per_ms - round(packet_number_per_ms)
+                    if round(packet_number_per_ms) == 0 and remainder < 1:
+                        remainder += packet_number_per_ms
+                        timestamp += 1
+                        continue
+
+                    if skip_flag:
+                        skip_flag = False
+                        continue
+                    remainder -= round(packet_number_per_ms) - packet_number_per_ms
                     if remainder > 1 or remainder == 1:
-                        repeated_times += 1
+                        timestamp += 1
                         remainder -= 1
                     elif remainder < -1 or remainder == -1:
-                        timestamp += 1
+                        skip_flag = True
                         remainder += 1
-                    for _ in range(repeated_times):
-                        file.write(str(timestamp) + '\n')
-
-def test():
-    duration_ms = 1000
-
-
-
+                    file.write(str(timestamp) + '\n')
 
 if __name__ == "__main__":
     df_measurements = parse_log("portal_measurements.csv")
